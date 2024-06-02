65b0df698ece9cb0dc58167f8dac2ad34124fad0
Huy317 <167590855+Huy317@users.noreply.github.com>
2024-06-02 14:31:54 +0700

    added save check

--- src/main/java/com/mycompany/oop_lab6/NewStudentDialog.java
+++ src/main/java/com/mycompany/oop_lab6/NewStudentDialog.java
@@ -16,6 +16,7 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
@@ -25,39 +26,43 @@ import javax.swing.JTextField;
  * @author GIA HUY
  */
 public class NewStudentDialog extends JDialog {
+
     private JTextField studentId, lastName, midName, firstName, yearOfBirth;
-    private JRadioButton male,female;
+    private JRadioButton male, female;
     private JComboBox<String> schoolState;
     private JButton btnSave, btnClear, btnCancel;
     private Student newStudent;
     private boolean isSaving;
+
     public Student getNewStudent() {
         return newStudent;
     }
-    public boolean isSaving(){
+
+    public boolean isSaving() {
         return this.isSaving;
     }
+
     public void setNewStudent(Student newStudent) {
         this.newStudent = newStudent;
     }
-    
+
     public NewStudentDialog() throws HeadlessException {
     }
 
     public NewStudentDialog(Frame owner, String title) {
         super(owner, title);
-        this.setSize(300,300);
+        this.setSize(300, 300);
         this.setLocation(200, 200);
-        
+
         this.setLayout(new GridLayout(5, 1));
-        
+
         JPanel panel1 = new JPanel(new FlowLayout());
         panel1.add(new JLabel("Student ID"));
         studentId = new JTextField(15);
         //studentId.setPreferredSize(new Dimension(50,20));
         panel1.add(studentId);
         this.add(panel1);
-        
+
         JPanel panel2 = new JPanel(new FlowLayout());
         panel2.add(new JLabel("Last-Mid-First Name"));
         lastName = new JTextField(5);
@@ -67,11 +72,11 @@ public class NewStudentDialog extends JDialog {
         panel2.add(midName);
         panel2.add(firstName);
         this.add(panel2);
-        
+
         JPanel panel3 = new JPanel(new FlowLayout());
         panel3.add(new JLabel("Year of Birth"));
         yearOfBirth = new JTextField(10);
-        
+
         ButtonGroup group = new ButtonGroup();
         male = new JRadioButton("Male");
         female = new JRadioButton("Female");
@@ -81,23 +86,24 @@ public class NewStudentDialog extends JDialog {
         panel3.add(male);
         panel3.add(female);
         this.add(panel3);
-        String[] data = {"Elementary School","Middle School","High School"};  
+        String[] data = {"Elementary School", "Middle School", "High School"};
         schoolState = new JComboBox(data);
-        
+
         this.add(schoolState);
-        
+
         JPanel panel4 = new JPanel(new FlowLayout());
         btnSave = new JButton("Save");
         btnClear = new JButton("Clear");
         btnCancel = new JButton("Canel");
         panel4.add(btnSave);
-        btnSave.addActionListener((e)->{
-            saveData();
-            isSaving = true;
-            this.setVisible(false);
+        btnSave.addActionListener((e) -> {
+            if (saveData()){
+                isSaving = true;
+                this.setVisible(false);
+            }
         });
         panel4.add(btnClear);
-        btnClear.addActionListener((e)->{
+        btnClear.addActionListener((e) -> {
             studentId.setText("");
             lastName.setText("");
             midName.setText("");
@@ -105,26 +111,30 @@ public class NewStudentDialog extends JDialog {
             yearOfBirth.setText("");
         });
         panel4.add(btnCancel);
-        btnCancel.addActionListener((e)->{
+        btnCancel.addActionListener((e) -> {
             isSaving = false;
             setVisible(false);
         });
-            
+
         this.add(panel4);
-        
+
         pack();
     }
-    
 
-    private void saveData(){
+    private boolean saveData() {
         String id = studentId.getText();
         String last = lastName.getText();
         String mid = midName.getText();
         String first = firstName.getText();
-        int yob = Integer.parseInt(yearOfBirth.getText());
-        String gender = male.isSelected() == true?"Male":"Female";
+        String yobText = yearOfBirth.getText();
+        if (id.equals("") || last.equals("") || mid.equals("") || first.equals("") || yobText.equals("")) {
+            JOptionPane.showMessageDialog(null, "Please input all infomation before saving", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        int yob = Integer.parseInt(yobText);
+        String gender = male.isSelected() == true ? "Male" : "Female";
         String school = schoolState.getSelectedItem().toString();
         newStudent = new Student(id, last, mid, first, yob, gender, school);
-        
+        return true;
     }
 }
